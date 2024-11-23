dceea11d77be6bf03053824d0879c9a7fa8f3d06
Francesco <francesco1896@gmail.com>
2024-11-23 04:44:00 +0100

    added dummy file

---
+++ back-nestjs/src/game/Types/game.exception.filter.ts
@@ -0,0 +1,27 @@
+
+import { ExceptionFilter, Catch, ArgumentsHost, InternalServerErrorException } from '@nestjs/common';
+
+@Catch(InternalServerErrorException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  
+  catch(exception: InternalServerErrorException) {
+
+  //   const ctx = host.switchToHttp();
+  //   const response = ctx.getResponse<Response>();
+  //   const request = ctx.getRequest<Request>();
+  //   const status = exception.getStatus();
+
+  //   response
+  //     .status(status)
+  //     .json({
+  //       statusCode: status,
+  //       timestamp: new Date().toISOString(),
+  //       path: request.url,
+  //     });
+  }
+}
+
+
+
+// @UseFilters(new HttpExceptionFilter())
+// export class CatsController {}
