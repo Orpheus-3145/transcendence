3d038986fb94bb5ba78c261a9fa2e5d3d5b1cbf2
Rares <rares.rares.cox@gmail.com>
2024-11-27 18:15:58 +0100

    working on displaying a channel with some test data

---
+++ front-vite/src/Pages/Channels/DisplayChannel.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {useState} from 'react';
+import { Box, TextField, Button, Typography } from '@mui/material';
+import { ChatMessage, UserRoles, UserProps, ChatSettings, ChatRoom, ChatProps } from '../../Layout/Chat/InterfaceChat';
+
+
