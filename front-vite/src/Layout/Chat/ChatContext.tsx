889d92df3b3da8c113dd436c187acc4656a399a9
Rares <rares.rares.cox@gmail.com>
2024-12-14 14:02:54 +0100

    created ChatContext to be shared among the Chat and ChannelsPage components

---
+++ front-vite/src/Layout/Chat/ChatContext.tsx
@@ -0,0 +1,90 @@
+import React, { createContext, useContext, useState } from 'react'
+import { ChatProps, ChatStatus } from './InterfaceChat';
+import { children } from 'cheerio/dist/commonjs/api/traversing';
+import { Group as GroupIcon } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+import { PersonAdd as PersonAddIcon} from '@mui/icons-material';
+
+
+interface ChatContextType {
+	chatProps: ChatProps;
+	setChatProps: React.Dispatch<React.SetStateAction<ChatProps>>;
+}
+
+const ChatContext = createContext<ChatContextType | undefined>(undefined);
+
+export const useChatContext = (): ChatContextType => {
+	const context = useContext(ChatContext);
+	if (!context) {
+		throw new Error('useChatContext must be used within a ChatProvider!');
+	}
+	return context;
+};
+
+export const ChatProvider: React.FC = ({ children }) => {
+	const [chatProps, setChatProps] = useState<ChatProps>({
+		chatRooms: [
+		  {
+			name: 'test channel',
+			icon: <GroupIcon />,
+			messages: [
+			  {
+				message: <Typography>Whazuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuup!</Typography>,
+				user: <Typography>User1</Typography>,
+				userPP: <Typography>img</Typography>,
+				timestamp: <Typography>20:00</Typography>,
+			  },
+			  {
+				message: <Typography>Whazuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuup!</Typography>,
+				user: <Typography>User2</Typography>,
+				userPP: <Typography>img</Typography>,
+				timestamp: <Typography>20:03</Typography>,
+			  },
+			],
+			settings: {
+			  icon: <PersonAddIcon />,
+			  type: 'public',
+			  password: null,
+			  users: [
+				  {
+					  name: 'Groot',
+					  role: 'Admin',
+					  email: 'iamgroot@avengers.com',
+					  password: '',
+					  icon: React.ReactElement ,
+  
+				  },
+				  {
+					  name: 'Cap',
+					  role: 'Guest',
+					  email: 'cap@avengers.com',
+					  password: '',
+					  icon: React.ReactElement ,
+  
+				  },
+				  {
+					  name: 'Hulk',
+					  role: 'Guest',
+					  email: 'hulk@avengers.com',
+					  password: '',
+					  icon: React.ReactElement ,
+  
+				  },
+			  ],
+			  owner: 'MYSELF',
+			},
+		  },
+		],
+		chatStatus: ChatStatus.ChannelsPage,
+		selected: null,
+		searchPrompt: null,
+	  });
+
+	return (
+		<ChatContext.Provider value={{ chatProps, setChatProps }} >
+			{children}
+		</ChatContext.Provider>
+	)
+}
+
+
