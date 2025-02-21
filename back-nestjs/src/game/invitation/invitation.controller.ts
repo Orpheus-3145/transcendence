2b4bd09d380ab65d02b496dd629200c0ad28748d
Fra <francesco.aru25@gmail.com>
2025-02-21 17:29:36 +0100

    ...

--- back-nestjs/src/game/invitation/invitation.controller.ts
+++ back-nestjs/src/game/invitation/invitation.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Query, Req, Res, UseFilters,  } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-// import { AuthService } from 'src/auth/auth.service';
 import { SessionExceptionFilter } from 'src/errors/exceptionFilters';
 import InvitationService from './invitation.service';
 
