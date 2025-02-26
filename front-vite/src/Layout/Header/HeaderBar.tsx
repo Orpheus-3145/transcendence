b9364617a1adc8891a8e40f79e92c5e7912ee2f2
XEDGit <muzio200@gmail.com>
2025-02-26 23:01:49 +0100

    Moved leaderboard and users to sidebar, notifications next to user profile picture

--- front-vite/src/Layout/Header/HeaderBar.tsx
+++ front-vite/src/Layout/Header/HeaderBar.tsx
@@ -1,67 +1,18 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { MenuDrawer } from './Navigation/Menu/MenuDrawer';
-import {Notification} from './Notification/index'
 import { Box, IconButton, Stack, Typography } from '@mui/material';
 import { useUser } from '../../Providers/UserContext/User';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
-import ContactsIcon from '@mui/icons-material/Contacts';
-import {  Tooltip } from '@mui/material';
-import Leaderboard from '../../Pages/Leaderboard';
-import GroupsIcon from '@mui/icons-material/Groups';
+import { Notification } from './Notification/index'
 
 export const Bar: React.FC = () => {
 	const { user } = useUser();
 	const navigate = useNavigate();
 	const theme = useTheme();
-	// const [showNotificationDot, setShowNotificationDot] = useState<Boolean>(false);
 
 	const navToProfile = () => { user && user.id && navigate(`/profile/${user.id}`) }
-	const navToAll = () => {navigate('/viewusers')}
-	const navToLeaderboard = () => {navigate('/leaderboard')}
-
-
-	let viewUsersWrapper = () =>
-	{
-		return (
-			<Tooltip title="View Users!" arrow>
-				<IconButton
-					onClick={() => navToAll()}
-					sx={{
-						position: 'absolute',
-						left: '-80px',
-						'&:hover': {
-						color: '#0c31df',
-						},
-					}}
-				>
-					<ContactsIcon />
-				</IconButton>
-          </Tooltip>
-		);
-	}
-
-	let leaderboardWrapper = () =>
-	{
-		return (
-			<Tooltip title="View Leaderboard!" arrow>
-				<IconButton
-					onClick={() => navToLeaderboard()}
-					sx={{
-						position: 'absolute',
-						left: '-25px',
-						'&:hover': {
-						color: '#0c31df',
-						},
-					}}
-				>
-					<GroupsIcon />
-				</IconButton>
-          </Tooltip>
-		);
-	}
 
 	let viewProfileWrapper = () =>
 	{
@@ -126,21 +77,10 @@ export const Bar: React.FC = () => {
 					borderBottom={`0.1em outset ${theme.palette.divider}`}
 				>
 					<MenuDrawer />
-					<Box
-						sx={{
-							position: 'fixed',
-							left: '50%',
-							transform: 'translateX(-50%)',
-							display: 'flex',
-							gap: '20px',
-							justifyContent: 'center',
-						}}
-					>
-						{viewUsersWrapper()}
-						{leaderboardWrapper()}
+					<Box display='flex' flexDirection='row'>
 						<Notification />
+						{viewProfileWrapper()}
 					</Box>
-					{viewProfileWrapper()}
 				</Stack>
 			</Box>
 		);
--- front-vite/src/Layout/Header/Notification/index.tsx
+++ front-vite/src/Layout/Header/Notification/index.tsx
@@ -512,8 +512,6 @@ export const Notification: React.FC = () => {
 					<IconButton
 						onClick={() => openBool()}
 						sx={{
-							position: 'absolute',
-							left: '30px',
 							color: color,
 							'&:hover': {
 								color: 'orange',
