a2371f438cc0824aefffb026a682b6264f1cc5d3
Dante Hussain <dhussain@f1r3s16.codam.nl>
2025-03-20 14:46:52 +0100

    fixed ui bug

--- front-vite/src/Layout/Chat/ChatContext.tsx
+++ front-vite/src/Layout/Chat/ChatContext.tsx
@@ -170,7 +170,7 @@ useEffect(() => {
 				if (!channel)
 					return (prevState);
 	
-				const updatedUsers = channel.settings.users.filter((item: UserProps) => item.id !== userId);
+				const updatedUsers = channel.settings.users.filter((item: UserProps) => item.id !== data.userId);
 				if (selectedChannel != null && selectedChannel.id === channel.id)
 				{
 					setSelectedChannel(null);
